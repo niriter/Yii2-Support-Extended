0ec6485fc37c6ec5a6a035b5b565d75b9a0a06d2
niriter <pro100.vce@gmail.com>
2026-07-15 23:15:19 +0200

    chore: remove obsolete repository files

--- TODO.md
+++
@@ -1,8 +0,0 @@
-Code completion in validation rules  
-Code completion for urls  
-Code completion for parameters  
-Properties generation  
-Phpdoc for variables in view
-
-To consider:  
-? Application parser for better experience  
--- src/com/nvlad/yii2support/common/YiiIndexKeys.java
+++
@@ -1,8 +0,0 @@
-package com.nvlad.yii2support.common;
-
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.stubs.StubIndexKey;
-
-public interface YiiIndexKeys {
-    StubIndexKey<String, PsiFile> VIEW = StubIndexKey.createIndexKey("yii2support.view.index");
-}
